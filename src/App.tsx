2148d2705275e5d5346d9770f47df59737345cad
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-20 06:17:30 +0200

    started work on the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div className="bg-kaguya bg-cover bg-center h-screen"></div>;
+  return (
+    <div className="bg-kaguya bg-cover bg-center h-screen text-white overflow-hidden">
+      <div className="border h-[calc(100vh-2.50rem)] border-cyan-500 m-5 ">
+        <div className="w-full bg-cyan-700/75 pl-1 text-sm h-fits leading-tight text-cyan-400">
+          DISPLAY
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
