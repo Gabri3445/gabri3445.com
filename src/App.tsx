ef68ec41ca0d17454f629c3a7fab23aa394ba236
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-20 06:19:01 +0200

    changed font size

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="bg-kaguya bg-cover bg-center h-screen text-white overflow-hidden">
       <div className="border h-[calc(100vh-2.50rem)] border-cyan-500 m-5 ">
-        <div className="w-full bg-cyan-700/75 pl-1 text-sm h-fits leading-tight text-cyan-400">
+        <div className="w-full bg-cyan-700/75 pl-1 text-xs h-fits leading-tight text-cyan-400">
           DISPLAY
         </div>
         <div></div>
