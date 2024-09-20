ef43338dffc6a9750adaed9117cd15dff0f7fd73
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-20 07:15:27 +0200

    disconnect button

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import ServerInfo from "./components/ServerInfo/ServerInfo";
 function App() {
   return (
     <div className="bg-kaguya bg-cover bg-center h-screen text-white overflow-hidden select-none">
-      <div className="border h-[calc(100vh-2.50rem)] border-cyan-500 m-5 ">
+      <div className="border h-[calc(100vh-2.50rem)] border-cyan-500 m-5 flex flex-col">
         <div className="w-full bg-cyan-700/75 pl-1 text-xs h-fits leading-tight text-cyan-400">
           DISPLAY
         </div>
-        <div>
+        <div className="grow flex flex-col">
           <ServerInfo />
-          <div className="ml-4 mt-20">
+          <div className="ml-6 mt-20 grow flex flex-col justify-between">
             <Button sideColor="bg-fuchsia-300" isBig={true} text="Test" />
+            <div className="mb-2">
+              <Button
+                sideColor="bg-[#6c1a49]"
+                isBig={false}
+                text="Disconnect"
+                onClick={() => {
+                  history.back();
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
