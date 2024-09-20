14b2bf78bcfb0cd7bc06ae4a727ff9a00ec0814f
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-20 06:54:56 +0200

    select none

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ServerInfo from "./components/ServerInfo/ServerInfo";
 
 function App() {
   return (
-    <div className="bg-kaguya bg-cover bg-center h-screen text-white overflow-hidden">
+    <div className="bg-kaguya bg-cover bg-center h-screen text-white overflow-hidden select-none">
       <div className="border h-[calc(100vh-2.50rem)] border-cyan-500 m-5 ">
         <div className="w-full bg-cyan-700/75 pl-1 text-xs h-fits leading-tight text-cyan-400">
           DISPLAY
