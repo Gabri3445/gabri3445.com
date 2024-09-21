7d35f3e397d41697b3091c9582fb68eabf2843ca
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-21 11:07:31 +0200

    customizable height and width for the buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,28 @@
 export interface ButtonProps {
   text: string;
   onClick?: () => void;
+  height?: string;
+  width?: string;
   isBig: boolean;
   sideColor: string;
 }
 
-function Button({ text, onClick, isBig, sideColor }: ButtonProps) {
+function Button({
+  text,
+  onClick,
+  height,
+  width,
+  isBig,
+  sideColor,
+}: ButtonProps) {
   return (
     <div
-      className="last:mb-0 mb-2 flex flex-row border border-white/75 w-96 bg-[#36313a]/70 hover:bg-[#59545c]/70 active:bg-[#252127]/70"
+      className={`last:mb-0 mb-2 flex flex-row border border-white/75 w-96  ${height ?? "h-fit"} ${width ?? "w-96"}
+      bg-[#36313a]/70 hover:bg-[#59545c]/70 active:bg-[#252127]/70`}
       onClick={onClick}
     >
       <div className={sideColor + " w-2.5 mr-1.5"}></div>
-      <div className={isBig ? "my-2" : "text-sm"}>{text}</div>
+      <div className={isBig ? "my-auto" : "text-sm my-auto"}>{text}</div>
     </div>
   );
 }
