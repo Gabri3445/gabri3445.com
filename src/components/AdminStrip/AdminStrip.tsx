f5d6318ad742f9c80093d9679594b04b0afbab24
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-21 13:14:51 +0200

    Admin strip component

---
+++ src/components/AdminStrip/AdminStrip.tsx
@@ -0,0 +1,19 @@
+export interface AdminStripProps {
+  error: boolean;
+}
+
+function AdminStrip({ error }: AdminStripProps) {
+  return (
+    <div
+      className={`w-full text-center ${error ? "bg-[#691144]/90" : "bg-[#fecafe]"}`}
+    >
+      <span className={error ? "text-[#bf0541]" : "text-black"}>
+        {error
+          ? "Insufficient Priviliges to Perform Operation"
+          : "You are the Administrator of this System"}
+      </span>
+    </div>
+  );
+}
+
+export default AdminStrip;
