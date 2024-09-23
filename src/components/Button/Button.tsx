c102a3534362eb6f2ec78f3cc73c1dc51bc71f44
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-23 18:07:52 +0200

    fixed width bug

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -17,7 +17,7 @@ function Button({
 }: ButtonProps) {
   return (
     <div
-      className={`last:mb-0 mb-2 flex flex-row border border-white/75 w-96  ${height ?? "h-fit"} ${width ?? "w-96"}
+      className={`last:mb-0 mb-2 flex flex-row border border-white/75 ${height ?? "h-fit"} ${width ?? "w-96"}
       bg-[#36313a]/70 hover:bg-[#59545c]/70 active:bg-[#252127]/70`}
       onClick={onClick}
     >
