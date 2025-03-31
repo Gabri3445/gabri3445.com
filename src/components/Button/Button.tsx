6e5461a86bb073e89ea4aaf54ba7f4be75e674c4
Gabriele Lopez <gabriele.ope@gmail.com>
2025-03-31 17:06:43 +0200

    remove border on last child prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,6 +6,7 @@ export interface ButtonProps {
   isBig: boolean;
   sideColor?: string;
   centered?: boolean;
+  addBorderLast?: boolean;
 }
 
 function Button({
@@ -16,10 +17,12 @@ function Button({
   isBig,
   sideColor,
   centered,
+  addBorderLast,
 }: ButtonProps) {
   return (
     <div
-      className={`last:mb-0 mb-2 flex flex-row border border-white/75 ${height ?? "h-fit"} ${width ?? "w-96"}
+      //last:mb-0
+      className={`${addBorderLast ? "" : "last:mb-0"} mb-2 flex flex-row border border-white/75 ${height ?? "h-fit"} ${width ?? "w-96"}
       bg-[#36313a]/70 hover:bg-[#59545c]/70 active:bg-[#252127]/70`}
       onClick={onClick}
     >
